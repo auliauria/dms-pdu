87a433a9b174ff47b6bc2bae785c43dc1b7ee58b
felcy <husnafelisacahyani@gmail.com>
2025-10-04 21:36:08 +0000

    edit: api.php

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get('/test', function() {
+    return response()->json(['message' => 'API is working']);
+});
 
 Route::post('/register-user', [UserController::class, 'register']);
 Route::post('/login-user', [UserController::class, 'login']);
 Route::post('/logout-user', [UserController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::get('/test', function() {
-    return response()->json(['message' => 'API is working']);
-});
+
